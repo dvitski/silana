27d25fb39850c4982c3b73b0800f1661e92f2dc6
Daniel Orr <daniel@dvitski.cc>
2026-09-08 13:46:38 +0100

    feat: 26.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 @file:Suppress("PropertyName")
 
 plugins {
-    id("cc.dvitski.gradle.fabric") version "2.0.0+fabric-loom.1.16"
+    id("cc.dvitski.gradle.fabric") version "2.0.0+fabric-loom.1.17"
 }
 
 version = "1.1.0"
@@ -9,9 +9,9 @@ group = "cc.dvitski"
 
 fabricDsl {
     modId.set("silana")
-    minecraftVersion.set("26.1.2")
-    loaderVersion.set("0.19.2")
-    fabricApiVersion.set("0.147.0+26.1.2")
-    kotlinVersion.set("2.2.21")
-    fabricKotlinVersion.set("1.13.7")
+    minecraftVersion.set("26.2")
+    loaderVersion.set("0.19.3")
+    fabricApiVersion.set("0.154.2+26.2")
+    kotlinVersion.set("2.4.0")
+    fabricKotlinVersion.set("1.13.12")
 }
--- src/client/kotlin/cc/dvitski/silana/data/server/BlockTagProvider.kt
+++ src/client/kotlin/cc/dvitski/silana/data/server/BlockTagProvider.kt
@@ -1,16 +1,16 @@
 package cc.dvitski.silana.data.server
 
 import cc.dvitski.silana.SilanaBlockTags
+import java.util.concurrent.CompletableFuture
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider
 import net.fabricmc.fabric.api.tag.convention.v2.ConventionalBlockTags
 import net.minecraft.core.HolderLookup
 import net.minecraft.tags.BlockTags
-import java.util.concurrent.CompletableFuture
 
 class BlockTagProvider(output: FabricPackOutput, future: CompletableFuture<HolderLookup.Provider>) : FabricTagsProvider.BlockTagsProvider(output, future) {
     override fun addTags(provider: HolderLookup.Provider) {
-        valueLookupBuilder(SilanaBlockTags.VEINABLE)
+        builder(SilanaBlockTags.VEINABLE)
             .addOptionalTag(ConventionalBlockTags.ORES)
             .forceAddTag(BlockTags.LOGS)
             .forceAddTag(BlockTags.LEAVES)
